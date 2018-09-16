30bccd41d0d107516121aed347cd471776d867ca
Unknown <danis994@gmail.com>
2018-09-16 21:33:59 +0530

    dw

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
     // tags: ["tag1", "tag2", "tag3"]
   };
 
@@ -20,6 +20,12 @@ class Counter extends Component {
           className="btn btn-secondery btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         {/* {this.renderTags()} */}
       </div>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,11 +10,36 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = counterId => {
+    console.log("Evenet handle called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
-          <Counter keys={counter.id} value={counter.value} id={counter.id} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
